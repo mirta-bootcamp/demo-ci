1d8aa9f8377755fff018f16e5763d07a5498de27
Mirta Arambulo <arambulomirta@gmail.com>
2020-06-10 09:27:07 -0400

    Added fucntion to divide two integers

--- src/main/java/Math.java
+++ src/main/java/Math.java
@@ -10,5 +10,15 @@ public class Math {
 	public static int sum(int x, int y) {
 		return x+y;
 	}
+	
+	/**
+	 * Divide 2 integers
+	 * @param x
+	 * @param y
+	 * @return the result as integer
+	 */
+	public static int divide(int x, int y) {
+		return (int) x/y;
+	}
 
 }
--- src/test/java/MathTest.java
+++ src/test/java/MathTest.java
@@ -8,5 +8,10 @@ class MathTest {
 	void testSum() {
 		assertEquals(2, Math.sum(1, 1));
 	}
+	
+	@Test
+	void testDivide() {
+		assertEquals(2, Math.divide(10, 5));
+	}
 
 }
